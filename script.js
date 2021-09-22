2c69c76e3da1940fd8f8bb5bd33e37d1786a883b
Johnson Pasanna <johnsonpasanna1997@gmail.com>
2021-09-22 21:48:48 +0530

    updated javascript

--- script.js
+++ script.js
@@ -91,3 +91,24 @@
 // const rest = new Map();
 // rest.set('John', 'Son');
 // console.log(rest);
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+console.log(modal);
+console.log(overlay);
+console.log(btnCloseModal);
+console.log(btnsShowModal);
+
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+btnCloseModal.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
